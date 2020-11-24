ea08cd0475faad1ec933cfdfde8271b1ca3bb6d3
isidak <isidak@gmail.com>
2020-11-24 10:54:38 +0200

    first commit

---
+++ src/app/pages/admin/pages/users/models/user-extended.model.spec.ts
@@ -0,0 +1,7 @@
+import { UserExtended } from './user-extended.model';
+
+describe('UserExtended', () => {
+  it('should create an instance', () => {
+    expect(new UserExtended()).toBeTruthy();
+  });
+});
---
+++ src/app/pages/admin/pages/users/models/user-extended.model.ts
@@ -0,0 +1,7 @@
+import {UserModel} from './user.model';
+
+export class UserExtendedModel extends UserModel {
+  address: string;
+  status: string;
+  image: string;
+}
